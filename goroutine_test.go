116bb219af442b83b831b6b31c665c5f230083b4
Andromeda <andromeda951@gmail.com>
2022-11-29 21:30:49 +0700

    add func TestManyGoroutine

--- goroutine_test.go
+++ goroutine_test.go
@@ -16,3 +16,16 @@ func TestCreateGoroutine(t *testing.T) {
 
 	time.Sleep(10 * time.Millisecond) // hapus ini bakal kelihatan hasilnya beda-beda
 }
+
+func DispalyNumber(number int) {
+	fmt.Println("Dispaly", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+
+	for i := 0; i <= 100000; i++ {
+		go DispalyNumber(i)
+	}
+
+	time.Sleep(10 * time.Second)
+}
