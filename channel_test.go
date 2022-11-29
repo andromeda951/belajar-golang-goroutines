41b36d8eb0aa1c74389f8cb8941c99b0605a4099
Andromeda <andromeda951@gmail.com>
2022-11-29 22:18:47 +0700

    add channel_test.go

---
+++ channel_test.go
@@ -0,0 +1,24 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Andromeda"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+
+}
