a7805c3d6662c2797e271f8a531d85184b84cf3d
Andromeda <andromeda951@gmail.com>
2022-11-29 20:55:13 +0700

    add func TestCreateGoroutine

---
+++ goroutine_test.go
@@ -0,0 +1,18 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Ups")
+
+	time.Sleep(10 * time.Millisecond) // hapus ini bakal kelihatan hasilnya beda-beda
+}
