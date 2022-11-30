85f9199429f409c06b5520ed58b38433cb7a430a
Andromeda <andromeda951@gmail.com>
2022-11-30 21:39:45 +0700

    add func TestBufferedChannel

--- channel_test.go
+++ channel_test.go
@@ -63,3 +63,33 @@ func TestInOutChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 }
+
+// kenapa di vs code ini tidak deadloack? hanya time out after 30s
+// func TestBufferedChannel(t *testing.T) {
+// 	channel := make(chan string)
+// 	defer close(channel)
+
+// 	channel <- "Eko"
+
+// 	time.Sleep(2 * time.Second)
+// 	fmt.Println("Selesai")
+// }
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Eko"
+		channel <- "Kurniawan"
+		channel <- "Khannedy"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Selesai")
+}
