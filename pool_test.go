d4455f281b2fafd4548b49042e73d88eddfe31af
Andromeda <andromeda951@gmail.com>
2022-12-07 14:02:38 +0700

    revisi func TestPool

--- pool_test.go
+++ pool_test.go
@@ -14,13 +14,23 @@ func TestPool(t *testing.T) {
 	pool.Put("Kurniawan")
 	pool.Put("Khannedy")
 
+	// Using Pointer
+	// firstName := "Eko"
+	// middleName := "Kurniawan"
+	// lastName := "Khannedy"
+
+	// pool.Put(&firstName)
+	// pool.Put(&middleName)
+	// pool.Put(&lastName)
+
 	for i := 0; i < 10; i++ {
 		go func() {
 			data := pool.Get()
 			fmt.Println(data)
+			time.Sleep(1 * time.Second) // add this line
 			pool.Put(data)
 		}()
 	}
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
 }
