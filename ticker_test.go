92d2329bd40bddbc608699b2e697a867ba7439f7
Andromeda <andromeda951@gmail.com>
2022-12-08 10:36:44 +0700

    add ticker_test.go

---
+++ ticker_test.go
@@ -0,0 +1,15 @@
+package belajar_golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+
+	for tick := range ticker.C {
+		fmt.Println(tick)
+	}
+}
