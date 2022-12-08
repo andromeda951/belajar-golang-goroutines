ed7ba3d90d2e9d3f325adb9c16feb9e43d8f6fed
Andromeda <andromeda951@gmail.com>
2022-12-08 11:17:41 +0700

    add func TestChangeThreadNumber

--- gomaxprocs_test.go
+++ gomaxprocs_test.go
@@ -29,3 +29,26 @@ func TestGetGomaxprocs(t *testing.T) {
 
 	group.Wait()
 }
+
+func TestChangeThreadNumber(t *testing.T) {
+	group := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go func() {
+			time.Sleep(3 * time.Second)
+			group.Done()
+		}()
+	}
+
+	totalCpu := runtime.NumCPU()
+	fmt.Println("CPU", totalCpu)
+
+	runtime.GOMAXPROCS(20)
+	totalThread := runtime.GOMAXPROCS(-1)
+	fmt.Println("Thread", totalThread)
+
+	totalGoroutine := runtime.NumGoroutine()
+	fmt.Println("Goroutine", totalGoroutine)
+
+	group.Wait()
+}
