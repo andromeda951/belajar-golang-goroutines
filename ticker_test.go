4dd742fddd6bc1e9af70c1bc9851718af0751501
Andromeda <andromeda951@gmail.com>
2022-12-08 10:43:40 +0700

    add ticker.Stop()

--- ticker_test.go
+++ ticker_test.go
@@ -9,7 +9,13 @@ import (
 func TestTicker(t *testing.T) {
 	ticker := time.NewTicker(1 * time.Second)
 
+	go func() {
+		time.Sleep(5 * time.Second)
+		ticker.Stop()
+	}()
+	
 	for tick := range ticker.C {
 		fmt.Println(tick)
 	}
+
 }
